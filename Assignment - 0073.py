a659d1bf6a71e1f1abab66ba3f775f7b7719f383
C7212-Murat <muratdemiray@gmail.com>
2021-01-18 20:40:25 +0300

    completed

---
+++ Assignment - 0073.py
@@ -0,0 +1,17 @@
+# C7212- Murat
+
+number = input("Please enter a positive integer: ")
+while not number.isdigit():
+    print ("It is an invalid entry. Don't use non-numeric, float, or negative values!")
+    number = input("Please enter a positive integer: ")
+
+sum = 0
+len = len(number)
+for i in range(0,len):
+    sum += int(number[i])**len
+
+if sum == int(number):
+    print(number,"is an Armstrong number")
+else:
+    print(number,"is not an Armstrong number")
+    
